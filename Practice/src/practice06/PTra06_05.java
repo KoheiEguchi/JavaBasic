b260f2885f679aea37c2ab3a0c57b6c00b7bfa0e
KoheiEguchi <ytictnaidar2642@gmail.com>
2018-03-16 17:42:26 +0900

    PTra06_07まで完了

--- Practice/src/practice06/PTra06_05.java
+++ Practice/src/practice06/PTra06_05.java
@@ -19,11 +19,12 @@ public class PTra06_05 {
 		 * ■■■■■
 		 * ■■■■■
 		 */
-		for(int i=1;i<=25;i++) {
-			System.out.print("■");
-		if(i%5==0) {
+		for (int i = 0; i < 5; i++) {
+			for (int j = 0; j < 5; j++) {
+				System.out.print("■");
+			}
 			System.out.println();
 		}
-		}
+
 	}
 }
