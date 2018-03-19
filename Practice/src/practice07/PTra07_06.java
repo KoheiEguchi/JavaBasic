0ad0a7dee24b68a30f1997ef564fa9d1e863e4d8
KoheiEguchi <ytictnaidar2642@gmail.com>
2018-03-19 11:55:53 +0900

    PTra07_06まで完了

--- Practice/src/practice07/PTra07_06.java
+++ Practice/src/practice07/PTra07_06.java
@@ -16,6 +16,8 @@ public class PTra07_06 {
 		};
 
 		// ★ 配列arrayに格納されている全ての数字の平均を出力してください
-
-	}
+		double sum1=array[0][0]+array[0][1]+array[0][2];
+		double sum2=array[1][0]+array[1][1]+array[1][2]+array[1][3];
+			System.out.println((sum1+sum2)/(array[0].length+array[1].length));
+		}
 }
