2d703cbd7e8452e26f88591f9750395d440cc3c7
KoheiEguchi <ytictnaidar2642@gmail.com>
2018-03-26 12:03:16 +0900

    PTra13_07まで完了

---
+++ Practice/src/practice13/common/SuperHero.java
@@ -0,0 +1,18 @@
+package practice13.common;
+
+public class SuperHero extends Hero {
+
+	Item equipment;
+
+	public int attack(){
+		return super.attack()+equipment.getAdditionalDamage();
+	}
+
+	public Item getEquipment() {
+		return this.equipment;
+	}
+	public void setEquipment(Item equipment) {
+		this.equipment=equipment;
+	}
+
+}
